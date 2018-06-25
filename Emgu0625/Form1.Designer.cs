3c22ebbef191e8501f329ae16e02a9b1ec53f73b
a53236717171 <532367171@qq.com>
2018-06-25 20:07:02 +0800

    ““emgu控制摄像头 成功“

---
+++ Emgu0625/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Emgu0625
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(54, 65);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(197, 123);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Emgu0625/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Emgu0625
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Capture cap;
+        private bool isProcess = false;
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            if (cap != null)
+            {
+                if (isProcess)
+                {
+                    Application.Idle -= new EventHandler(ProcessFram);
+                    button1.Text = "stop!";
+                }
+                else
+                {
+                    Application.Idle += new EventHandler(ProcessFram);
+                    button1.Text = "start!";
+                }
+                isProcess = !isProcess;
+            }
+            else
+            {
+                try
+                {
+                    cap = new Emgu.CV.Capture();
+                }
+                catch (NullReferenceException expt)
+                {
+                    MessageBox.Show(expt.Message);
+                }
+            }
+        }
+
+        private void ProcessFram(object sender, EventArgs arg)
+        {
+            imageBox1.Image = cap.QueryFrame();
+        }
+    }
+}
